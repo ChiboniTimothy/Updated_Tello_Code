cd738147d80220cf1bbe42b824027a8973203fca
Aliyon-dev <117938808+Aliyon-dev@users.noreply.github.com>
2022-12-01 17:30:59 +0200

    First development

---
+++ drone.py
@@ -0,0 +1,22 @@
+
+from djitellopy import Tello
+tello = Tello()
+
+tello.connect()
+tello.streamon()
+tello.takeoff()
+tello.move_left(100)
+tello.flip_forward()
+tello.flip_back()
+tello.rotate_counter_clockwise(90)
+tello.flip_forward()
+tello.flip_back()
+
+
+
+#tello.move_left(100)
+#tello.rotate_counter_clockwise(90)
+#tello.move_forward(100)
+#tello.flip_forward()
+#tello.flip_back()
+#tello.land()
